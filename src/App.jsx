f612c98a644b076489678200fca49ec4f35edf47
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-06-16 10:10:31 +0100

    imported slider.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Slider from "./components/Slider"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Slider/>
+    </>
   )
 }
 
 export default App
+
+// but...
+// 				don't worry
+// 				because...
+// 				God has your back
+// 				and
+// 				this year will be better
+// 				and I hope
+// 				you'll find...
+// 				happiness along the way
+// 				keep your spirit up
+// 				enjoy every single moment...
+// 				that you experience today
+// 				fill it with your most beautiful smile
+// 				and make it the best memory..
+// 				lastly...
+// 				I'd like to wish you one more time
+// 				a very happy birthday Sapthesh V
