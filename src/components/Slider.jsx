8cde997f29fb2680be9b6df14ae90d08281468f3
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-06-16 13:46:04 +0100

    created a slider of pecs pictures

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,60 +1,67 @@
 // import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import One from '../images/One.jpg';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import One from "../images/One.jpg";
+import Two from "../images/two.jpg";
+import Three from "../images/three.jpg";
+import Four from "../images/four.jpg";
+import Five from "../images/five.jpg";
+import Six from "../images/six.jpg";
+import Seven from "../images/seven.png";
+import Eight from "../images/eight.png";
 
-
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 const Slider = () => {
   return (
-   <>
-   <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide >
-          <img className='h-[250px] w-full'   src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'    src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'    src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'    src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'   src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'   src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'   src={One} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[250px] w-full'   src={One} />
-        </SwiperSlide>
-      </Swiper>
-   </>
-  )
-}
+    <>
+      <div className="mt-10 px-5">
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={One} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Two} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Three} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Four} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Five} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Six} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Seven} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="h-[250px] w-full" src={Eight} />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+};
 
-export default Slider
+export default Slider;
