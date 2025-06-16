6314176469c5049fc7b70b3971d1d85ab1352926
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-06-16 13:45:33 +0100

    added a happy birthday banner as header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../assets/banner.png'
+
+const Header = () => {
+  return (
+    <div>
+        <img src={Banner} alt="" srcset="" />
+    </div>
+  )
+}
+
+export default Header
