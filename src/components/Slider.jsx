c33c10a9d3f818cc0f9e248221e2cd6bd0691491
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-07-12 09:31:13 +0100

    made the carousel autoplay

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import './Slider.css'
+// Import Autoplay CSS if you have specific styles for it, though often not strictly necessary for basic functionality
+// import "swiper/css/autoplay";
+
 import One from "../images/one.jpg";
 import Two from "../images/two.jpg";
 import Three from "../images/three.jpg";
@@ -13,15 +15,17 @@ import Six from "../images/six.jpg";
 import Seven from "../images/seven.png";
 import Eight from "../images/eight.png";
 
-import { EffectCoverflow, Pagination } from "swiper/modules";
+// Import Autoplay module
+import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
 
 const Slider = () => {
   return (
     <>
       <div className="mt-10 px-5 text-center text-xl">
-        <div className="mb-2">
-          
-        </div>
+        {/* <div className="mb-2">
+          I did not want to tip you off, so I picked pictures of you I liked
+          from the internet and made this
+        </div> */}
         <Swiper
           effect={"coverflow"}
           grabCursor={true}
@@ -36,34 +40,39 @@ const Slider = () => {
           }}
           pagination={{
             clickable: true,
-            dynamicBullets: true, // This is key for the 3D effect on pagination
+            dynamicBullets: true,
           }}
-          modules={[EffectCoverflow, Pagination]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true, 
+          }}
+          modules={[EffectCoverflow, Pagination, Autoplay]} 
           className="mySwiper"
         >
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={One} />
+            <img className="h-[300px] w-full" src={One} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Two} />
+            <img className="h-[300px] w-full" src={Two} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Three} />
+            <img className="h-[300px] w-full" src={Three} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Four} />
+            <img className="h-[300px] w-full" src={Four} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Five} />
+            <img className="h-[300px] w-full" src={Five} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Six} />
+            <img className="h-[300px] w-full" src={Six} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Seven} />
+            <img className="h-[300px] w-full" src={Seven} />
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[250px] w-full" src={Eight} />
+            <img className="h-[300px] w-full" src={Eight} />
           </SwiperSlide>
         </Swiper>
       </div>
